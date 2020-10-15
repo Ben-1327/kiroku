05c47be9a6ec578c6d180d6cdb471e2f732e4b96
Ben1327 <ff.walker1327@gmail.com>
2020-10-15 16:16:18 +0900

    [Add] post new page, create controller sample

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,21 @@
 class PostsController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def new
+    @posts = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.category_id = current_user.categories.find_by(id: params[:category_id]).id
+  	if @post.save
+  		redirect_to user_post_path(@post), notice: "投稿に成功しました!"
+  	else
+  		@user = User.find(params[:id])
+  		render 'users/show'
+  	end
+  end
+
 end
