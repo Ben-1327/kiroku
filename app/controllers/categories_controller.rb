3e8e19b82b833133d75f4b29cdbc70c48685afa0
Ben1327 <ff.walker1327@gmail.com>
2020-10-16 18:20:12 +0900

    [Add] category new page sample

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,32 @@
 class CategoriesController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def index
+    @user = User.find(params[:user_id])
+    @categories = @user.categories.page(params[:page]).reverse_order
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.user_id = current_user.id
+  	if @category.save
+  		redirect_to user_categories_path(@user), notice: "登録に成功しました!"
+  	else
+  		@user = User.find(params[:id])
+  		render 'users/show'
+  	end
+  end
+
+
+  private
+
+  def category_params
+    params.require(:category).permit(:user_id, :name)
+  end
+
 end
