3a0077f30b9da310b09f1aca534e5ef9c169f119
Ben1327 <ff.walker1327@gmail.com>
2020-10-17 12:46:26 +0900

    [Add] post edit page sample

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,19 @@ class PostsController < ApplicationController
   	end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+  	if @post.update(post_params)
+  		redirect_to user_post_path(current_user, @post), notice: "更新に成功しました!"
+  	else
+  		render :edit
+  	end
+  end
+
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
